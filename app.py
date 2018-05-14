bd3a58ae770df90b20d055af636b5fbbf95fe166
Jonathan LOQUET <Jonthan.LT@gmail.com>
2018-05-14 17:56:34 +0200

    Change password

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -6,3 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/rpi/password', methods=['PUT'])
+def change_password():
+    req_data = request.get_json(force=True)  # force=True will make sure this works even if a client does not specify application/json
+    password = req_data['password']  # or whatever key you have in your json
+    return 'sudo passwd fred < {}'.format(password)
