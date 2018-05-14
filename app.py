66fb1cba23376398ef68fbbb4bcb5717033fa62c
Jonathan LOQUET <Jonthan.LT@gmail.com>
2018-05-14 18:45:15 +0200

    Change password

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def change_password():
     req_data = request.get_json(force=True)
     password = req_data['password']
     p = subprocess.Popen(["echo -e '{}\n{}' | sudo passwd pi".format(password, password)], shell=True, stdout=subprocess.PIPE)
-    return "password updated successfully" in p.communicate()
+    return {"status": "password updated successfully" in p.communicate()}
