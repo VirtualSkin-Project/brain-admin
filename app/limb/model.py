e1b93fd12ddbb043602d589bbdca994fdd5c4cfd
Jonathan LOQUET <Jonthan.LT@gmail.com>
2018-05-17 20:29:38 +0200

    Remove ssh_key

--- app/limb/model.py
+++ app/limb/model.py
@@ -2,14 +2,13 @@ from app import db
 
 
 class Limb(db.Model):
-    cols = ['id', 'name', 'ip', 'area', 'sub_area', 'ssh_key']
+    cols = ['id', 'name', 'ip', 'area', 'sub_area']
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     ip = db.Column(db.String(120))
     area = db.Column(db.String(120))
     sub_area = db.Column(db.String(120))
-    ssh_key = db.Column(db.String)
 
     def __repr__(self):
         return str({
@@ -17,6 +16,5 @@ class Limb(db.Model):
             "name": self.name,
             "ip": self.ip,
             "area": self.area,
-            "sub_area": self.sub_area,
-            "ssh_key": self.ssh_key
+            "sub_area": self.sub_area
         })
