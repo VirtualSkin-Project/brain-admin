d2c8a79547e268c2c388ae8e9359480031151f8b
virtualskin <37671628+virtualskin@users.noreply.github.com>
2018-05-19 20:21:09 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -23,10 +23,11 @@ sudo cp brain-manager /etc/init.d/brain-manager
 sudo update-rc.d brain-manager defaults
 sudo systemctl start brain-manager.service
 
-sudo apt install -y npm
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt install -y nodejs
 sudo npm install -g @angular/cli
 
-git clone https://github.com/VirtualSkin-Project/brain-front.git
-cd brain-front
-npm install package.json
-ng serve --host 0.0.0.0 --port 8000&
+#git clone https://github.com/VirtualSkin-Project/brain-front.git
+#cd brain-front
+#npm install package.json
+#ng serve --host 0.0.0.0 --port 8000&
