0d36ba917a4b338d4d55b5af40dd68c49e05623f
Jonathan LOQUET <Jonthan.LT@gmail.com>
2018-05-20 09:42:50 +0000

    Change setup.sh and add front OK

--- setup.sh
+++ setup.sh
@@ -22,7 +22,9 @@ curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt install -y nodejs
 sudo npm install -g @angular/cli
 
-#git clone https://github.com/VirtualSkin-Project/brain-front.git
-#cd brain-front
-#npm install package.json
-#ng serve --host 0.0.0.0 --port 8000&
+if [ ! -d ./brain-front ] ; then
+	git clone https://github.com/VirtualSkin-Project/brain-front.git
+fi
+cd brain-front
+npm install package.json
+sudo ng serve --host 0.0.0.0 --port 80 --disable-host-check &
