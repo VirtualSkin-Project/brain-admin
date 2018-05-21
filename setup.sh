5a6d0c33181b925ff61ea78143d9891102674965
Jonathan LOQUET <Jonthan.LT@gmail.com>
2018-05-21 22:20:04 +0000

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo `which virtualenv`
-
 sudo apt install nginx
 sudo pip install virtualenv
 
@@ -11,7 +9,7 @@ venv/bin/pip install -r requirements.txt
 
 venv/bin/python script.py
 
-if [ -f /etc/init.d/brain-manager ] ; then
+if [[ -f /etc/init.d/brain-manager ]]; then
 	sudo rm /etc/init.d/brain-manager
 fi
 sudo cp brain-manager /etc/init.d/brain-manager
@@ -22,14 +20,14 @@ curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt install -y nodejs
 sudo npm install -g @angular/cli
 
-if [ ! -d ./front ] ; then
+if [[ ! -d ./front ]]; then
 	git clone https://github.com/VirtualSkin-Project/brain-front.git front
 fi
 cd front
 npm install
 
 PROCESS_PID="`ps -ef | grep 'ng serve --host 0.0.0.0 --port 80 --disable-host-check' | head -1 | awk '{print $2}'`"
-if [[ ! $PROCESS_PID -eq "" ]]; then
+if [[ ! $PROCESS_PID -eq "" ]]; then
 	sudo kill $PROCESS_PID
 fi
 sudo ng serve --host 0.0.0.0 --port 80 --disable-host-check &
